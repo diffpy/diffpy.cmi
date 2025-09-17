d0e17cfdf781953cfb24f7e1ad519857c9eef08b
Caden Myers <cjm2304@columbia.edu>
2025-09-17 11:24:15 -0400

    fix typo in comment

--- tests/test_ch07.py
+++ tests/test_ch07.py
@@ -8,7 +8,7 @@ chapter_dir = Path(__examples_dir__) / chapter
 example_scripts = list(chapter_dir.rglob("*.py"))
 
 
-# Runs all example scripts in chapter 7, skips files in main() is not defined.
+# Runs all example scripts in chapter 7, skips files if main() is not defined.
 # Passes if script runs without error.
 @pytest.mark.parametrize("script_path", example_scripts, ids=lambda p: p.name)
 def test_ch03_examples(script_path):
