f9a14c30b8ed80dcdcf907e26fd05b460ec65bd5
Caden Myers <cjm2304@columbia.edu>
2025-10-06 16:13:36 -0400

    test for listing available examples

--- tests/test_packsmanager.py
+++ tests/test_packsmanager.py
@@ -1,9 +1,35 @@
-# from diffpy.cmi.packsmanager import PacksManager
+import pytest
+
+from diffpy.cmi.packsmanager import PacksManager
 
 
-def test_available_examples():
+@pytest.mark.parametrize(
+    "expected_dict",
+    [
+        {
+            "pdf": [
+                "ch03NiModelling",
+                "ch06RefineCrystalStructureGen",
+                "ch07StructuralPhaseTransition",
+                "ch08NPRefinement",
+            ]
+        }
+    ],
+)
+def test_available_examples(expected_dict):
     """Test that available_examples returns a dict."""
-    assert False
+    pkmg = PacksManager()
+    returned_dict = pkmg.available_examples()
+    expected_pack = list(expected_dict.keys())
+    returned_pack = list(returned_dict.keys())
+    for pack in expected_pack:
+        assert pack in returned_pack, f"{pack} not found in returned packs."
+        expected_examples = expected_dict[pack]
+        returned_examples = returned_dict.get(pack, [])
+        for ex in expected_examples:
+            assert (
+                ex in returned_examples
+            ), f"{ex} not found under pack {pack}."
 
 
 def test_print_info():
