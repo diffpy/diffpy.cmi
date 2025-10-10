5ac0487a56a3d8b41ffb94198de9630f22c41f1c
Caden Myers <cjm2304@columbia.edu>
2025-10-10 09:54:00 -0400

    rm unrelated function

--- tests/conftest.py
+++ tests/conftest.py
@@ -92,14 +92,6 @@ def example_cases(tmp_path_factory):
     yield root_temp_dir
 
 
-@pytest.fixture(scope="session")
-def target_dir(tmp_path_factory):
-    """Create a temporary directory to serve as the target for copying
-    examples."""
-    target_directory = tmp_path_factory.mktemp("copy_target")
-    yield target_directory
-
-
 @pytest.fixture(scope="session", autouse=True)
 def use_headless_matplotlib():
     """Force matplotlib to use a headless backend during tests."""
